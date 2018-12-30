7089c24ce414050c49bb0665deaece6a3d3339d0
nandy <nandy_wong@126.com>
2018-12-30 21:04:47 +0800

    增加注释编写sql示例

--- src/main/java/com/nandy007/web/dao/UserMapper.java
+++ src/main/java/com/nandy007/web/dao/UserMapper.java
@@ -1,7 +1,22 @@
 package com.nandy007.web.dao;
 
+import java.util.List;
+
 import com.nandy007.web.core.Mapper;
 import com.nandy007.web.model.User;
 
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
 public interface UserMapper extends Mapper<User> {
+
+    //如果实例对象中的属性名和数据表中字段名不一致，用@Result注解进行说明映射关系，我在这里只是告诉你怎么写
+    @Select("SELECT * FROM user")
+    // @Results({
+    //         @Result(property = "username", column = "username"),
+    //         @Result(property = "sex",  column = "sex"),
+    //         @Result(property = "address",column = "address")
+    // })
+    List<User> selectAll();
 }
